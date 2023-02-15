74328bfe42a349efa8f8d07926596eb70bdea9ca
coordova <grcordova@gmail.com>
2023-02-15 18:38:51 -0400

    Laravel ChirpsInertia - 07 - Notifications & Events

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
@@ -13,10 +15,15 @@ class Chirp extends Model
         'message',
     ];
 
+    // configuramos nuestro modelo 'Chirp' para despachar los eventos
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
     /*----------------------------------------------------*/
     // DB - User -||-----o|E Chirp - relacion User con Chirp
     /*----------------------------------------------------*/
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
